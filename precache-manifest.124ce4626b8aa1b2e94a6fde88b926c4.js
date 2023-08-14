70b7cc5f06e1e034505fd16e7532d57436ec47ec
Daniel <dani54196@gmail.com>
2023-08-14 19:15:48 -0300

    Updates

---
+++ precache-manifest.124ce4626b8aa1b2e94a6fde88b926c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987e1979e9b7aa5ff89a3eed68faba84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df41ef7289c2a2724295",
+    "url": "/static/css/main.206c68bc.chunk.css"
+  },
+  {
+    "revision": "af19868669149cc06f23",
+    "url": "/static/js/2.a24ed293.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a24ed293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df41ef7289c2a2724295",
+    "url": "/static/js/main.2ce57984.chunk.js"
+  },
+  {
+    "revision": "b3642bbd661725da4950",
+    "url": "/static/js/runtime-main.3c554f96.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/moremis15/precache-manifest.0a81d872beab859b64b8c8a93570fa4e.js"
+  "/precache-manifest.124ce4626b8aa1b2e94a6fde88b926c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moremis15/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
